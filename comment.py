b6e99fc40a89b2dbee262cb6cd4b583f827ec6e9
hovanhoa <hovanhoaa1@gmail.com>
2024-05-24 12:38:15 +0700

    feat: add get list followers

--- comment.py
+++ comment.py
@@ -3,7 +3,7 @@ from twitter.scraper import Scraper
 import json
 
 
-## sign-in with cookies
+# sign-in with cookies
 scraper = Scraper(
     cookies={
         "ct0": "39acaaddec983399cc6c4a37e83d4f9e2539cad87d2acebf822c68672ac707bb11c12ff0cbae1656995d6cbe6d2cb7646b9228bf99dfc7ce06b06dfdf46406d62c2764ba4c0772bb91a082c91a279b94",
